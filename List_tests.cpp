f27392e67b393eb0f0aab16bae83a691a70f3c1d
nhicksn <nhicksn@umich.edu>
2022-11-11 13:35:12 -0500

    a

--- List_tests.cpp
+++ List_tests.cpp
@@ -7,9 +7,22 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(test_stub) {
-    // Add test code here
-    ASSERT_TRUE(true);
+TEST(test_empty) {
+    List<int> List;
+    List.push_back(5);
+    assert(!List.empty());
+    List.pop_front();
+    assert(List.empty());
+}
+
+TEST(test_size) {
+    List<double> List;
+    List.push_back(5);
+    assert(List.size() == 1);
+    List.push_front(2);
+    assert(List.size() == 2);
+    List.pop_back();
+    assert(List.size() == 1);
 }
 
 TEST_MAIN()
